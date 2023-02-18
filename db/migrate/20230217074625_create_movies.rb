5424532a7e8c557c3342335f4521ea682e34ba09
Littlezealous <zulaikha92@gmail.com>
2023-02-18 15:58:04 +0800

    input controller for movie,list and bookmarks

--- db/migrate/20230217074625_create_movies.rb
+++ db/migrate/20230217074625_create_movies.rb
@@ -2,10 +2,9 @@ class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.text :overview
+      t.string :overview
       t.string :poster_url
-      t.integer :rating
-      t.references :movie, null: false, foreign_key: true
+      t.decimal :rating
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_063120) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_073351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.text "overview"
-    t.string "poster_url"
-    t.integer "rating"
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
     t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["movie_id"], name: "index_movies_on_movie_id"
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
-  add_foreign_key "movies", "movies"
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.string "poster_url"
+    t.decimal "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
